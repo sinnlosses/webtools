05e3e3a3db481377b0ecf28152ebe4db9abc1775
fuji <fujiyoshiyuki@gmail.com>
2022-09-18 15:31:19 +0000

    サイドバーをスクロール可能にし、リストにユニークキーを追加

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -13,52 +13,51 @@ type ChildType = {
   link: string,
 };
 
-const ParentContent = (props: ParentType) => {
+const ParentListItem = (props: ParentType) => {
   return (
-    <li className='m-2' key={props.title}>
-    <Link href={props.link}>
-      <a className={`flex p-2 rounded hover:bg-slate-700 cursor-pointer`}>
-        <div className='mx-2'>{props.icon}</div>
-        <div className='mx-2'>{props.title}</div>
-      </a>
-    </Link>
-  </li>
+    <li className='m-2'>
+      <Link href={props.link}>
+        <a className={`flex p-2 rounded hover:bg-slate-700 cursor-pointer`}>
+          <div className='mx-2'>{props.icon}</div>
+          <div className='mx-2'>{props.title}</div>
+        </a>
+      </Link>
+    </li>
   );
 };
 
-const ChildContent = (props: ChildType) => {
+const ChildListItem = (props: ChildType) => {
   return (
-    <li className='m-2' key={props.title}>
-    <Link href={props.link}>
-      <a className={`flex p-2 rounded hover:bg-slate-700 cursor-pointer`}>
-        <div className='mx-2 opacity-50'>{props.title}</div>
-      </a>
-    </Link>
-  </li>
+    <li className='ml-10 m-2'>
+      <Link href={props.link}>
+        <a className={`flex p-2 rounded hover:bg-slate-700 cursor-pointer`}>
+          <div className='mx-2 opacity-50'>{props.title}</div>
+        </a>
+      </Link>
+    </li>
   );
 };
 
 export default function Sidebar() {
   return (
-    <aside className='bg-slate-900 md:w-72 hidden md:block'>
+    <aside className='bg-slate-900 h-screen md:w-72 hidden md:block overflow-y-scroll'>
       <nav>
         <ul>
           {/* ホーム */}
-          <ParentContent title={Home.title} icon={Home.icon} link={Home.link} />
+          <ParentListItem key={Home.title} title={Home.title} icon={Home.icon} link={Home.link} />
 
           {/* Webツール */}
           {ToolContents.map((pvalue, key) => (
-            <>
-              <ParentContent title={pvalue.title} icon={pvalue.icon} link={pvalue.link} />
-              <div className='ml-10'>
-                {pvalue.children.map((cvalue, key) => (
-                    <ChildContent title={cvalue.title} link={cvalue.link} />
-                ))}
-              </div>
-            </>
+            <div key={pvalue.title}>
+              <ParentListItem key={pvalue.title} title={pvalue.title} icon={pvalue.icon} link={pvalue.link} />
+              {pvalue.children.map((cvalue, key) => (
+                  <ChildListItem key={cvalue.title} title={cvalue.title} link={cvalue.link} />
+              ))}
+            </div>
           ))}
+
           {/* 設定 */}
-          <ParentContent title={Setting.title} icon={Setting.icon} link={Setting.link} />
+          <ParentListItem key={Setting.title} title={Setting.title} icon={Setting.icon} link={Setting.link} />
         </ul>
       </nav>
     </aside>
